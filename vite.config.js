a43ed64d11a7abfb913c7eaaeaee9155f6bec9e6
Ramilqq_lab <jazzduem@yandex.ru>
2024-01-01 18:25:26 +0200

    remove admin_css_js

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import laravel from 'laravel-vite-plugin';
 export default defineConfig({
     plugins: [
         laravel({
-            input: ['resources/css/app.css', 'resources/js/app.js', 'resources/css/app_admin.css', 'resources/js/app_admin.js'],
+            input: ['resources/css/app.css', 'resources/js/app.js'],
             refresh: true,
         }),
     ],
